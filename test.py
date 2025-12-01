5fa78fe48a3be7bb5908ef62bc3298219a3bbbf1
Mandoquest <norwin@yandaal.de>
2025-12-01 20:25:02 +0100

    update
    player coins

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from funktionen.inv_interface import add_item, remove_item, get_inventory
+from funktionen.inv_interface import add_item
 
-
-inventory = get_inventory("hanno", "sword")
-print(inventory)
+add_item("959526368272605184", "MandoCoins", 5300)
+add_item("1190989022743498756", "MandoCoins", 730)
+add_item("984039672474914827", "MandoCoins", 16222)
+add_item("792415676790079509", "MandoCoins", 24190)
+add_item("811503580968124437", "MandoCoins", 2868)
+add_item("1071070814264373258", "MandoCoins", 2030)
